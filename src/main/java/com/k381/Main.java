3ea60f801fc714511bb09879f0b7e9ea8471f140
LastHero91 <kam.06@mail.ru>
2018-10-15 22:56:19 +0300

    second commit

--- src/main/java/com/k381/Main.java
+++ src/main/java/com/k381/Main.java
@@ -2,10 +2,10 @@ package com.k381;
 
 public class Main {
     public static void main(String[] args) {
-        test(args.length == 1 ? args[0] : "No name!");
+        test(args.length == 1 ? args[0] : "No name");
     }
 
     private static void test(String name) {
-        System.out.println("Hello " + name + "!");
+        System.out.println("Hello " + name + "!\n"+System.getProperty("agr_prop", "Tester"));
     }
 }
