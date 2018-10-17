6767b16961076aa639829f7e81b395e918a66016
LastHero91 <kam.06@mail.ru>
2018-10-17 23:10:07 +0300

    Third commit

--- src/main/java/com/k381/Main.java
+++ src/main/java/com/k381/Main.java
@@ -1,8 +1,11 @@
 package com.k381;
 
+import com.google.common.collect.Lists;
+
 public class Main {
     public static void main(String[] args) {
-        test(args.length == 1 ? args[0] : "No name");
+        //test(args.length == 1 ? args[0] : "No name");
+        test(Lists.asList("Sully", args).toString());
     }
 
     private static void test(String name) {
